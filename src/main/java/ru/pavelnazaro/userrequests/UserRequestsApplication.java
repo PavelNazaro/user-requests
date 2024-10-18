4c0094dd345e3469684125a066715ade2dd4fde3
pavel <pnazarov@atypicaldigital.com>
2024-10-18 21:32:37 +0300

    Initial commit

---
+++ src/main/java/ru/pavelnazaro/userrequests/UserRequestsApplication.java
@@ -0,0 +1,13 @@
+package ru.pavelnazaro.userrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRequestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pavelnazaro/userrequests/UserRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pavelnazaro.userrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
