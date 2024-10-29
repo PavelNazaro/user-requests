f01318828ffd003eba29e25cd06ad670ac59c552
pavel <pnazarov@atypicaldigital.com>
2024-10-29 17:53:39 +0300

    Remove native @Query

--- src/main/java/ru/pavelnazaro/userrequests/repositories/RequestRepository.java
+++ src/main/java/ru/pavelnazaro/userrequests/repositories/RequestRepository.java
@@ -3,12 +3,12 @@ package ru.pavelnazaro.userrequests.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.pavelnazaro.userrequests.enums.RequestStatus;
 import ru.pavelnazaro.userrequests.models.Request;
 import ru.pavelnazaro.userrequests.models.User;
+
+import java.util.List;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
@@ -17,7 +17,6 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
 
     Page<Request> findByStatus(RequestStatus status, Pageable pageable);
 
-    @Query("SELECT r FROM Request r WHERE r.status=:status AND LOWER(r.user.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    Page<Request> findByUserNameContainingAndStatus(@Param("name") String name, @Param("status") RequestStatus status, Pageable pageable);
+    Page<Request> findByUser_NameContainingIgnoreCaseAndStatus(String username, RequestStatus status, Pageable pageable);
 
 }
